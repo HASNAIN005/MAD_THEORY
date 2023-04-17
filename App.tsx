39a144800538c3bfd2697aed3c8479245ee9b973
HASNAIN AHMED <113760859+HASNAIN005@users.noreply.github.com>
2023-04-17 04:44:47 -0700

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,18 +1,27 @@
 import React, { FC } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 interface Props {
   name: string;
 }
 
 const Greeting: FC<Props> = ({ name }) => {
-  const capitalized = name.toUpperCase();
-
   return (
-    <View>
-      <Text>Hello, {capitalized}!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, {name}!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
 
 export default Greeting;
