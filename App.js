f4603930d2dadb32b88864438bebdec1f0d5b629
HASNAIN AHMED <113760859+HASNAIN005@users.noreply.github.com>
2023-05-28 18:22:11 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,49 +1,58 @@
+// Import necessary dependencies
 import React from 'react';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View,Text,Button } from 'react-native';
 
-
-
-
+// Create stack navigator
 const Stack = createStackNavigator();
 
-const ProfileScreen = () => {
-    return (
-      <View>
-        <Text>Profile Screen</Text>
-    
-      </View>
-    );
-  };
-  const LoginScreen = () => {
-    return (
-      <View>
-        <Text>Login Screen</Text>
-      </View>
-    );
-  };
-  
-  const SignupScreen = () => {
-    return (
-      <View>
-        <Text>Signup Screen</Text>
-                                                                                                   
-      </View>
-    );
-  };
-  
+// Define screen components
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+      <Button
+        title="Sign Up"
+        onPress={() => navigation.navigate('Signup')}
+      />
+    </View>
+  );
+}
 
-const App = () => {
+function LoginScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Screen</Text>
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function SignupScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Signup Screen</Text>
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+// Create app component
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default App;
