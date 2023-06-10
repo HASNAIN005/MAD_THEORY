fefb79a77766557d5c5faceea3c1633c78f7571b
HASNAIN AHMED <113760859+HASNAIN005@users.noreply.github.com>
2023-06-10 09:16:04 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,58 +1,145 @@
-// Import necessary dependencies
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// Create stack navigator
 const Stack = createStackNavigator();
 
-// Define screen components
-function HomeScreen({ navigation }) {
+const ProfileScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <Button
+        title="Signup"
+        onPress={() => navigation.navigate('Signup')}
+      />
       <Button
         title="Login"
         onPress={() => navigation.navigate('Login')}
       />
-      <Button
-        title="Sign Up"
-        onPress={() => navigation.navigate('Signup')}
+    </View>
+  );
+};
+
+const SignupScreen = ({ navigation }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = async () => {
+    const userInfo = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    try {
+      await AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
+      console.log('User information stored successfully');
+      navigation.navigate('Profile');
+    } catch (error) {
+      console.log('Error storing user information:', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
       />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Signup" onPress={handleSignup} />
     </View>
   );
-}
+};
 
-function LoginScreen({ navigation }) {
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const storedUserInfo = await AsyncStorage.getItem('userInfo');
+
+      if (storedUserInfo) {
+        const userInfo = JSON.parse(storedUserInfo);
+
+        if (email === userInfo.email && password === userInfo.password) {
+          console.log('Login successful');
+          navigation.navigate('Profile');
+        } else {
+          console.log('Invalid login credentials');
+        }
+      } else {
+        console.log('User information not found');
+      }
+    } catch (error) {
+      console.log('Error retrieving user information:', error);
+    }
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Screen</Text>
-      <Button title="Go Back" onPress={() => navigation.goBack()} />
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Button title="Login" onPress={handleLogin} />
     </View>
   );
-}
+};
 
-function SignupScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Signup Screen</Text>
-      <Button title="Go Back" onPress={() => navigation.goBack()} />
-    </View>
-  );
-}
-
-// Create app component
-function App() {
+const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Navigator initialRouteName="Profile">
+        <Stack.Screen name="Profile" component={ProfileScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 10,
+    padding: 10,
+  },
+});
 
 export default App;
